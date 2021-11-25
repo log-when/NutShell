578bae9f43a6d54ec432a59ff33112c3e09ab42b
liuyic00 <liuyic00@gmail.com>
2021-11-25 11:53:02 +0800

    fix: use FlushableQueueIO with new flush port
    fit the new QueueIO, added a optional IO for a flushable Queue

--- src/main/scala/nutcore/frontend/IFU.scala
+++ src/main/scala/nutcore/frontend/IFU.scala
@@ -155,7 +155,7 @@ class IFU_ooo extends NutCoreModule with HasResetVector {
     val brIdx = Output(Vec(4, Bool()))
   }
   val mcpResultQueue = Module(new FlushableQueue(new MCPResult, entries = 4, pipe = true, flow = true))
-  mcpResultQueue.io.flush := io.redirect.valid || io.bpFlush
+  mcpResultQueue.io.flush.get := io.redirect.valid || io.bpFlush
   mcpResultQueue.io.enq.valid := mcp.io.valid
   mcpResultQueue.io.enq.bits.redirect := mcp.io.out
   mcpResultQueue.io.enq.bits.brIdx := mcp.io.brIdx
