5f48767e3ba4b190042f93d9a5e60de9b0fa9f79
liuyic00 <liuyic00@gmail.com>
2021-12-10 17:55:29 +0800

    fix: move difftest test code into if

--- src/main/scala/nutcore/frontend/IDU.scala
+++ src/main/scala/nutcore/frontend/IDU.scala
@@ -207,24 +207,23 @@ class IDU(implicit val p: NutCoreConfig) extends NutCoreModule with HasInstrType
 
   val checkpoint_id = RegInit(0.U(64.W))
 
-  // debug runahead
-  val runahead = Module(new DifftestRunaheadEvent)
-  runahead.io.clock         := clock
-  runahead.io.coreid        := 0.U
-  runahead.io.valid         := io.out(0).fire
-  runahead.io.branch        := decoder1.io.isBranch
-  runahead.io.pc            := io.out(0).bits.cf.pc
-  runahead.io.checkpoint_id := checkpoint_id
-  when(runahead.io.valid && runahead.io.branch) {
-    checkpoint_id := checkpoint_id + 1.U // allocate a new checkpoint_id
-  }
-  io.out(0).bits.cf.isBranch := decoder1.io.isBranch
-  io.out(0).bits.cf.runahead_checkpoint_id := checkpoint_id
-  // when(runahead.io.valid) {
-  //   printf("fire pc %x branch %x inst %x\n", runahead.io.pc, runahead.io.branch, io.out(0).bits.cf.instr)
-  // }
-
   if (!p.FPGAPlatform) {
+    // debug runahead
+    val runahead = Module(new DifftestRunaheadEvent)
+    runahead.io.clock         := clock
+    runahead.io.coreid        := 0.U
+    runahead.io.valid         := io.out(0).fire
+    runahead.io.branch        := decoder1.io.isBranch
+    runahead.io.pc            := io.out(0).bits.cf.pc
+    runahead.io.checkpoint_id := checkpoint_id
+    when(runahead.io.valid && runahead.io.branch) {
+      checkpoint_id := checkpoint_id + 1.U // allocate a new checkpoint_id
+    }
+    io.out(0).bits.cf.isBranch := decoder1.io.isBranch
+    io.out(0).bits.cf.runahead_checkpoint_id := checkpoint_id
+    // when(runahead.io.valid) {
+    //   printf("fire pc %x branch %x inst %x\n", runahead.io.pc, runahead.io.branch, io.out(0).bits.cf.instr)
+    // }
     BoringUtils.addSource(decoder1.io.isWFI | decoder2.io.isWFI, "isWFI")
   }
 }
