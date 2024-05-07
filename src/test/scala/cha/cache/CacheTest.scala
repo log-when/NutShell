6f6e434025d121d55a5df9fec1c123c2f253a5f4
log-when <13121802177@163.com>
2024-05-07 18:54:51 +0800

    rename names of asserts/assumes

--- src/test/scala/cha/cache/CacheTest.scala
+++ src/test/scala/cha/cache/CacheTest.scala
@@ -58,20 +58,20 @@ class CacheStage1Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
 
   val arrayReadReqReady = cio.metaReadBus.req.ready && cio.dataReadBus.req.ready
   // s1_assume_2: if no request to s2, then metaArray and dataArray can eventually be accessed 
-  // inducted by s2_goal_1, s3_goal_1, s3_goal_2, array_goal_1
+  // inducted by s2_assert_1, s3_assert_1, s3_assert_2, array_assert_1
   chaAssume(this,"(G !s1ReqValid) -> F G arrayReadReqReady")
   
   val s1ReqFire = cio.out.fire()
   // s1_assume_3: if request to s2 does not make a handshake, then metaArray and dataArray can eventually be accessed 
-  // inducted by s2_goal_3, s3_goal_1, s3_goal_2, array_goal_1
+  // inducted by s2_assert_3, s3_assert_1, s3_assert_2, array_assert_1
   chaAssume(this,"(G !s1ReqFire) -> F G arrayReadReqReady")
 
   val dataArrayReadValid = cio.dataReadBus.req.valid
-  // s1_goal_1: if request to s2 does not make a handshake, then there will be no read request to dataArray
+  // s1_assert_1: if request to s2 does not make a handshake, then there will be no read request to dataArray
   // depend on s1_assume_3 (might also former assumption)
   // chaAssert(this, "(G !s1ReqFire) -> F G !dataArrayReadValid")
 
-  // s1_final_goal: request form core will trigger request for s2
+  // s1_goal: request form core will trigger request for s2
   // deps on s1_assume_1 and s1_assume_2, can be proven 
   chaAssert(this,"coreReqValid -> F s1ReqValid")
 }
@@ -107,21 +107,21 @@ class CacheStage2Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
   val s1ReqValid = pio.in.valid
   val s2ReqValid = s2.io.out.valid
   val s2ReqReady = s2.io.out.ready
-  // s2_goal_1: if no request from s1, there will be no request to s3 or s3 is always unready
+  // s2_assert_1: if no request from s1, there will be no request to s3 or s3 is always unready
   // no deps, can be proven
   // chaAssert(this, "(G !s1ReqValid) -> ((F G !s2ReqValid) || G !s2ReqReady)")
   
   val s2ReqFire = s2.io.out.fire()
-  // s2_goal_2: request to s3 will keep high until getting handshake
+  // s2_assert_2: request to s3 will keep high until getting handshake
   // no deps, guaranteed by pipeConnect between s1 and s2, can be proven
   // chaAssert(this, "s2ReqValid -> ((s2ReqValid U s2ReqFire) || G s2ReqValid)")
 
   val s1ReqFire = pio.in.fire()
-  // s2_goal_3: if request from s1 does not make a handshake, there will be no request to s3 or s3 is always unready
+  // s2_assert_3: if request from s1 does not make a handshake, there will be no request to s3 or s3 is always unready
   // no_deps, can be proven
   // chaAssert(this, "(G !s1ReqFire) -> ((F G !s2ReqValid) || G !s2ReqReady)")
 
-  // s2_goal_4: if request from s2 does not make a handshake, request from s1 will not make a handshake eventually
+  // s2_assert_4: if request from s2 does not make a handshake, request from s1 will not make a handshake eventually
   // no_deps, can be proven
   // chaAssert(this, "(G !s2ReqFire) -> F G !s1ReqFire")
 
@@ -131,13 +131,13 @@ class CacheStage2Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
   val s2ValidHit = s2.io.out.valid && s2.io.out.bits.hit
   val reqTag = s2.io.out.bits.req.addr.asTypeOf(addrBundle).tag
   val consistIndex = !s2ValidHit || (s2ForwardingTag === reqTag)
-  // s2_goal_5: if request to s3 is valid and the request meets a hit, then the output tag is the same as its in request 
+  // s2_assert_5: if request to s3 is valid and the request meets a hit, then the output tag is the same as its in request 
   // no_deps, can be proven
   // chaAssert(this, "consistIndex")
 
   val s3IsMmio = s2.io.out.bits.mmio && s2.io.out.valid
   val reqIsMmio = AddressSpace.isMMIO(s2.io.out.bits.req.addr) && s2.io.out.valid
-  // s2_goal_6: s2 will set the mmio-bit true <=> the request is a mmio request 
+  // s2_assert_6: s2 will set the mmio-bit true <=> the request is a mmio request 
   // no_deps, can be proven
   // chaAssert(this, "(!s3IsMmio || reqIsMmio) && (!reqIsMmio || s3IsMmio)")
 
@@ -164,7 +164,7 @@ class CacheStage2Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
   val metaWriteAddr = Cat(metaWriteTag, metaWriteIndex, 0.U(OffsetBits.W))
   val metaWriteNotMMIO = !AddressSpace.isMMIO(metaWriteAddr) || !metaWriteValid
 
-  // s2_goal_7: if mmio data will not be read or written to the cacheArray for k cyles from the beginning,
+  // s2_assert_7: if mmio data will not be read or written to the cacheArray for k cyles from the beginning,
   //            then all valid mmio requsets will encounter a cache miss at the kth cyle
   // no_deps, can be proven
   chaAssert(this, "!((metaReadsNotMMIO && metaWriteNotMMIO) U (!s2ForwardingNotMMIO  && (metaReadsNotMMIO && metaWriteNotMMIO)))", true)
@@ -177,18 +177,18 @@ class CacheStage2Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
   // --- another layer begins ---//
   // this assumption is conducted by some assertions in this stage before
   // s2_assume_1: mmio data will always not be read or written to the cacheArray
-  // inducted by s2_goal_7, s3_goal_5, array_goal_3, induction by time
+  // inducted by s2_assert_7, s3_assert_5, array_assert_3, induction by time
   // chaAssume(this, "metaReadsNotMMIO && metaWriteNotMMIO")
 
   val s3_mmio = s2.io.out.valid && s2.io.out.bits.mmio
   val s3_hit = s2.io.out.valid && s2.io.out.bits.hit
   
-  // s2_goal_8: all mmio requsts will encounter a cache miss
+  // s2_assert_8: all mmio requsts will encounter a cache miss
   // depends on s2_assume_1, can be proven
   // chaAssert(this, "!s3_mmio || !s3_hit")
   // --- another layer ends ---//
 
-  // s2_final_goal: request form s1 will trigger request to s3
+  // s2_goal: request form s1 will trigger request to s3
   // no deps, can be proven
   chaAssert(this, "s1ReqValid -> F s2ReqValid")
 }
@@ -280,13 +280,13 @@ class CacheStage3Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
   val s2ReqValid = pio.in.valid
   val s2ReqFire = pio.in.fire
   // s3_assume_8 : request to s3 will keep high until getting handshake
-  // verified by s2_goal_2: note that it is an overlapping implication
+  // verified by s2_assert_2: note that it is an overlapping implication
   chaAssume(this, "s2ReqValid -> ((s2ReqValid U s2ReqFire) || G s2ReqValid)")
 
   val s3ReqValid = cio.in.resp.valid
   val dataArrayReadReady = cio.dataReadBus.req.ready
   // s3_assume_9 : if no request to CPU, then metaArray and dataArray can eventually be accessed 
-  // inducted by s3_goal_3, s2_goal_4, s1_goal_1, s3_goal_4, array_goal_2
+  // inducted by s3_assert_3, s2_assert_4, s1_assert_1, s3_assert_4, array_assert_2
   chaAssume(this, "(G !s3ReqValid) -> F G dataArrayReadReady")
   
   val s2ForwardingMetas = pio.in.bits.metas
@@ -296,30 +296,30 @@ class CacheStage3Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
   val reqTag = pio.in.bits.req.addr.asTypeOf(addrBundle).tag
   val consistIndex = !s2ValidHit || (s2ForwardingTag === reqTag)
   // s3_assume_10: if request to s3 is valid and the request meets a hit, then the output tag is the same as its in request 
-  // verified in s2_goal_5
+  // verified in s2_assert_5
   chaAssume(this, "consistIndex")
   
   val s3IsMmio = pio.in.bits.mmio && pio.in.valid
   val reqIsMmio = AddressSpace.isMMIO(pio.in.bits.req.addr) && pio.in.valid
   // s3_assume_11: s2 will set the mmio-bit true <=> the request is a mmio request  
-  // verified in s2_goal_6
+  // verified in s2_assert_6
   chaAssume(this, "(!s3IsMmio || reqIsMmio) && (!reqIsMmio || s3IsMmio)")
 
   val arrayWriteReqValid = cio.metaWriteBus.req.valid || cio.dataWriteBus.req.valid
-  // s3_goal_1: if no request from s2, there will be no request to write metaArray and dataArray
+  // s3_assert_1: if no request from s2, there will be no request to write metaArray and dataArray
   // depend on s3_assume_1, s3_assume_2, s3_assume_4: partially ensure the run of state mechine, can be proven
   // chaAssert(this, "(G !s2ReqValid) -> F G !arrayWriteReqValid")
 
   val s2ReqReady = pio.in.ready
-  // s3_goal_2: if s3 is always unready, there will be no request to write metaArray and dataArray
+  // s3_assert_2: if s3 is always unready, there will be no request to write metaArray and dataArray
   // depend on: s3_assume_1 ~ s3_assume_4, s3_assume_6, s3_assume_8, can be proven
   // chaAssert(this, "(G !s2ReqReady) -> F G !arrayWriteReqValid")
   
-  // s3_goal_3: if no response to cpu, s2 and s3 will no be able to make handshake eventually
+  // s3_assert_3: if no response to cpu, s2 and s3 will no be able to make handshake eventually
   // take all the assumptions, can be proven
   // chaAssert(this, "(G !s3ReqValid) -> F G !s2ReqFire")
 
-  // s3_goal_4: if no response to cpu, there will be no request to write metaArray and dataArray
+  // s3_assert_4: if no response to cpu, there will be no request to write metaArray and dataArray
   // take all the assumptions, can be proven
   // chaAssert(this, "(G !s3ReqValid) -> F G !arrayWriteReqValid")
 
@@ -330,7 +330,7 @@ class CacheStage3Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
   val metaWriteAddr = Cat(metaWriteTag, metaWriteIndex, 0.U(OffsetBits.W))
   val metaWriteNotMMIO = !AddressSpace.isMMIO(metaWriteAddr) || !metaWriteValid
 
-  // s3_goal_5: if all valid mmio requsets will encounter a cache miss  for k cyles from the beginning,
+  // s3_assert_5: if all valid mmio requsets will encounter a cache miss  for k cyles from the beginning,
   //            then  mmio data will not be written to the cacheArray at the kth cyle
   // chaAssert(this, "!(s2ForwardingNotMMIO U ! metaWriteNotMMIO)", true)
 
@@ -339,10 +339,10 @@ class CacheStage3Prop(implicit val cacheConfig: CacheConfig) extends CacheModule
   val s3_mmio = pio.in.valid && pio.in.bits.mmio
   val s3_hit = pio.in.valid && pio.in.bits.hit
   // s3_assume_12: all mmio requsts will encounter a cache miss
-  // verified in s2_goal_8
+  // verified in s2_assert_8
   chaAssume(this, "!s3_mmio || !s3_hit")
   
-  // s3_final_goal: request form s2 will trigger response to cpu
+  // s3_goal: request form s2 will trigger response to cpu
   // depend on the all assume statement (in)directly
   chaAssert(this, "s2ReqValid -> F s3ReqValid")
   // --- another layer ends ---//
@@ -375,13 +375,13 @@ class CacheArrayProp(implicit val cacheConfig: CacheConfig) extends CacheModule{
 
   val arrayWriteReqValid = cio.s3_metaWriteBus.req.valid || cio.s3_dataWriteBus.req.valid
   val arrayReadReqReady = cio.s1_metaReadBus.req.ready & cio.s1_dataReadBus.req.ready
-  // array_goal_1: if there is always no write request to array,  array will be ready to read eventually
+  // array_assert_1: if there is always no write request to array,  array will be ready to read eventually
   // no deps, can be proven 
   // chaAssert(this, "(G !arrayWriteReqValid) -> F G arrayReadReqReady")
 
   val dataArrayReadValid = cio.s1_dataReadBus.req.valid
   val dataArrayReadReady = cio.s3_dataReadBus.req.ready
-  // array_goal_2: 
+  // array_assert_2: 
   // no deps, can be proven 
   // chaAssert(this, "(G !dataArrayReadValid & !arrayWriteReqValid) -> F G dataArrayReadReady")
 
@@ -411,7 +411,7 @@ class CacheArrayProp(implicit val cacheConfig: CacheConfig) extends CacheModule{
   val metaReadNotMMIO_3 = !AddressSpace.isMMIO(metaReadAddr_3)
   val metaReadsNotMMIO = metaReadNotMMIO_0 & metaReadNotMMIO_1 & metaReadNotMMIO_2 & metaReadNotMMIO_3
 
-  // array_goal_3: if input follows that there is no write with mmio addr, then the result of read request is not about mmio
+  // array_assert_3: if input follows that there is no write with mmio addr, then the result of read request is not about mmio
   // no deps, can be proven ()
   chaAssert(this, "!(metaWriteNotMMIO U !metaReadsNotMMIO)", true)
 
@@ -430,7 +430,7 @@ class CacheArrayProp(implicit val cacheConfig: CacheConfig) extends CacheModule{
 
   // val temp2 = Cat(temp, metaReadIndex, 0.U(OffsetBits.W))
   // val temp3 = !AddressSpace.isMMIO(temp2)
-  // // array_goal_4
+  // // array_assert_4
   // chaAssert(this, "!(metaWriteNotMMIO U !temp3)", true)
 
 }
